7a009b59acd1f43b78428edfa082edca2b6fd645
regulardrama12 <152436140+regulardrama12@users.noreply.github.com>
2023-12-15 10:55:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+let firstName = "Amangeldy ";
+let lastName = "Temirlan";
+document.write(firstName + lastName)
+
+let a = 8;
+let b = 4;
+let c = 2;
+let sum = a + b;
+document.write(sum )
+let raz = a - b;
+document.write(raz )
+let um = a * b;
+document.write(um )
+let del = a / b;
+document.write(del )
+let ost = a % b;
+document.write(ost )
+
+let isTrue = true;
+let isFalse = false;
+document.write(isTrue && isFalse)
+document.write(isTrue || isFalse)
+document.write(!isTrue)
+
+let per = 3;
+per++;
+document.write(per)
+let vtor = 3;
+vtor--;
+document.write(vtor)
+
+let name = "Amangeldy Temirlan";
+let  greeting = "Hello";
+let  message = greeting + name;
+document.write(message)
+
+let prefix = "Дядь  Даня ";
+let middle = "пошлите ";
+let suffix = "в CS 2";
+let vse = prefix + middle + suffix;
+document.write(vse)
+
+let  num1 = 6;
+let  num2 = 8;
+let result = num1 + num2;
+document.write(result)
+
+const qwerty = "Привет, Мир!";
+console.log(qwerty. toUpperCase());
+
+
+const poiu = "Прощай, Мир!";
+console.log(poiu.toLowerCase());
